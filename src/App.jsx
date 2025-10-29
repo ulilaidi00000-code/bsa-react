ef740d8579d00597ae1909f5bef2fce4773d3dec
ulilaidi00000-code <ulilaidi00000@gmail.com>
2025-10-29 18:57:53 +0700

    Kedua

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import './App.css'
 
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-
 import Home from './pages/Home'
 
 function App() {
   return (
     <div>
-      <Navbar/>
       <Home/>
-      <Footer/>
     </div>
   )
 }
---
+++ src/data/HomeSection.jsx
@@ -0,0 +1,4 @@
+export const homeSection = {
+    image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&amp;w=1770&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    hero: ''
+}
